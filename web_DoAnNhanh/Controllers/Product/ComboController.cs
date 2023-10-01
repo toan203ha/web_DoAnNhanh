83da3e4ae6cdf1890b0eb695b9b5beed2c8490fa
beoxx <giabao2005bbb@gmailcom>
2023-10-01 15:38:07 +0700

    layout_food-drinks-combo

---
+++ web_DoAnNhanh/Controllers/Product/ComboController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_DoAnNhanh.Controllers
+{
+    public class ComboController : Controller
+    {
+        // GET: Combo
+        public ActionResult ViewCombo()
+        {
+            return View();
+        }
+    }
+}
---
+++ web_DoAnNhanh/Controllers/Product/DrinksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_DoAnNhanh.Controllers
+{
+    public class DrinksController : Controller
+    {
+        // GET: Drinks
+        public ActionResult ViewDrinks()
+        {
+            return View();
+        }
+    }
+}
