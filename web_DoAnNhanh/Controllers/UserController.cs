4fc7c96764aa1fb62f5655464082193bd27f28c4
Pontax <toan30112003@gmail.com>
2023-10-01 13:19:49 +0700

    Login

---
+++ web_DoAnNhanh/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_DoAnNhanh.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult ViewKhuyenmai()
+        {
+            return View();
+        }
+    }
+}
